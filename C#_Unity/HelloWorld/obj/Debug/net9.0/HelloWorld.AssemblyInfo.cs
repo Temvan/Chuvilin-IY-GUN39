a56f7970be8398ef3fa5f1259b234e8646da1816
Temvan <johnchuvilin@gmail.com>
2025-09-20 01:10:44 +0300

    Logic_Operations

--- C#_Unity/HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ C#_Unity/HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03d902b948de77a5e68f2f85e93770b39017718f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0894e22d19dc49dd2ba254736975e9a5cef51835")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
